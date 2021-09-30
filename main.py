2f75fe6aae7d55980ff6241296f5dfcbc26c1705
Subhalingam D <subhalingam.d@gmail.com>
2021-09-30 17:12:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import sys
-from utils import read_file_and_split
+from utils import read_train_file, read_test_file, write_predictions
 from preprocessing import preprocess
-import numpy as np
 from constants import LENGTH_THRESH, RANDOM_SEED
 
-from sklearn.pipeline import make_pipeline
+from sklearn.utils import shuffle
+from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC, LinearSVC
-from sklearn.metrics import f1_score, accuracy_score
-from sklearn.pipeline import FeatureUnion, Pipeline
-from sklearn.base import BaseEstimator, TransformerMixin
+# from sklearn.svm import SVC, LinearSVC
 # from sklearn.decomposition import TruncatedSVD
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import PredefinedSplit
+# from sklearn.pipeline import FeatureUnion
+# from sklearn.metrics import f1_score, accuracy_score
+from sklearn.base import BaseEstimator, TransformerMixin
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.model_selection import PredefinedSplit
 
-# from qexpand import prob_rerank_method
+import pickle
 
 
 class ExclamationCount(BaseEstimator, TransformerMixin):
@@ -38,122 +38,74 @@ class QuestionMarkCount(BaseEstimator, TransformerMixin):
         return self
 
 
-class UrlCount(BaseEstimator, TransformerMixin):
+def test(model_dir, in_path, out_path):
+    X_test = read_test_file(in_path)
 
-    def transform(self, X, **transform_params):
-        count = [[min(str(x).count("http"), LENGTH_THRESH//4)] for x in X]
-        return count
+    try:
+        model = None
+        with open(model_dir+"/model.pkl", 'rb') as f:
+            model = pickle.load(f)
 
-    def fit(self, X, y=None, **fit_params):
-        return self
+        y_test = model.predict(X_test)
+        y_test = [y*4 for y in y_test]
 
+        write_predictions(out_path, y_test)
 
-class MentionCount(BaseEstimator, TransformerMixin):
-
-    def transform(self, X, **transform_params):
-        count = [[min(str(x).count("@"), LENGTH_THRESH//2)] for x in X]
-        return count
-
-    def fit(self, X, y=None, **fit_params):
-        return self
+    except:  # noqa: E722
+        print("Could not load model. Creating dummy output.")
+        write_predictions(out_path, [4]*len(X_test))
 
 
-if __name__ == '__main__':
-    mode = sys.argv[1]
-    data_path = sys.argv[2]
-    (X_train, y_train), (X_val, y_val), (X_test, y_test) = read_file_and_split(data_path)
+def train(data_dir, model_dir):
 
-    # X_train, y_train = X_train[:100], y_train[:100]
-    # X_val, y_val = X_val[:10], y_val[:10]
-    # X_test, y_test = X_test[:10], y_test[:10]
+    data_path = "training.csv"
+    if data_dir != "":
+        data_path = data_dir + '/' + data_path
 
-    for i in range(len(X_train)):
-        X_train[i] = preprocess(X_train[i])
-        if (i)%100000==0:
-            print(f"iter {i}")
-    for i in range(len(X_val)):
-        X_val[i] = preprocess(X_val[i])
-    for i in range(len(X_test)):
-        X_test[i] = preprocess(X_test[i])
-
-    tfidf = TfidfVectorizer(
-                    # preprocessor=preprocess,
-                    ngram_range=(1, 2),
-                    max_df=0.75,
-                    min_df=5,
-                    )
-
-    grid_params = [
-        # {
-        #     "features__tfidf__ngram_range": [(1, 2)],
-        #     "features__tfidf__max_df": [0.75, 0.85, 1.0],
-        #     "features__tfidf__min_df": [5, 7, 10],
-        #     "classifier__penalty": ['l1', 'l2'],
-        #     "classifier__C": [0.01, 0.1, 0.5, 1, 2, 10],
-        #     "classifier__solver": ["saga"]
-        # },
-        {
-        #    "features__tfidf__ngram_range": [(1, 2)],
-        #    "features__tfidf__max_df": [0.75],
-        #    "features__tfidf__min_df": [5],
-            "classifier__penalty": ['elasticnet'],
-            "classifier__C": [0.5, 1, 2],
-            "classifier__solver": ["saga"],
-            "classifier__l1_ratio": [0.2, 0.4, 0.6, 0.8, 1.0]
-        }
-        # {
-        #     "features__tfidf__ngram_range": [(1, 2)],
-        #     "features__tfidf__max_df": [0.75, 0.85, 1.0],
-        #     "features__tfidf__min_df": [5, 7, 10],
-        #     "classifier__penalty": ['l2'],
-        #     "classifier__C": [0.01, 0.1, 0.5, 1, 2, 10],
-        #     "classifier__solver": ["lbfgs"]
-        # }
-    ]
-
-    # Create a list where train data indices are -1 and validation data indices are 0
-    split_index = [-1]*len(X_train)+[0]*len(X_val)
-    print(sum(split_index))
-
-    tfidf.fit(X_train, y_train)
-    X = tfidf.transform(X_train+X_val)
-    X_test = tfidf.transform(X_test)
-
-    print(X.shape, X_test.shape)
-
-    # Use the list to create PredefinedSplit
-    pds = PredefinedSplit(test_fold=split_index)
+    X_train, y_train = read_train_file(data_path)
+    X_train, y_train = shuffle(X_train, y_train, random_state=RANDOM_SEED)
 
     model = Pipeline(steps=[
             # ("features", FeatureUnion([
-            #     ("tfidf", TfidfVectorizer(
-            #         # preprocessor=preprocess,
-            #         # ngram_range=(1, 2),
-            #         # max_df=0.75,
-            #         # min_df=5,
-            #         )),
+            ("tfidf", TfidfVectorizer(
+                preprocessor=preprocess,
+                ngram_range=(1, 2),
+                max_df=0.75,
+                min_df=5,
+                )),
             #     # ("excl", ExclamationCount()),
             #     # ("qmark", QuestionMarkCount()),
-            #     # ("url", UrlCount()),
-            #     # ("mention", MentionCount()),
             # ])),
-            # ("svd", TruncatedSVD(n_components=500, random_state=RANDOM_SEED)),
-            ("classifier", LogisticRegression(max_iter=10000, random_state=RANDOM_SEED))
+            # ("classifier", LinearSVC(max_iter=10000, dual=False, random_state=RANDOM_SEED))
+            ("classifier", LogisticRegression(
+                solver="lbfgs",
+                penalty="l2",
+                C=1.5,
+                max_iter=10000,
+                random_state=RANDOM_SEED,
+                n_jobs=-1
+                ))
             ], verbose=3)
 
-    model = GridSearchCV(model, grid_params, cv=pds, return_train_score=True, verbose=3, n_jobs=2)
-    model = model.fit(X, y_train+y_val)
+    model = model.fit(X_train, y_train)
 
-    # model = model.fit(X_train, y_train)
+    with open(model_dir+"/model.pkl", 'wb') as f:
+        pickle.dump(model, f)
 
-    print(model.best_estimator_)
-    print("*"*10)
-    print(model.cv_results_)
 
-    y_pred = model.predict(X_test)
-    print(f1_score(y_test, y_pred, average=None))
-    print(accuracy_score(y_test, y_pred))
+if __name__ == '__main__':
+    if len(sys.argv) < 2: raise ValueError("Missing mode (arg 1). Expecting [train|test]")
+    mode = sys.argv[1]
 
-    # y_pred = model.predict(X_train[:50000])
-    # print(f1_score(y_train[:50000],y_pred,average=None))
-    # print(accuracy_score(y_train[:50000],y_pred))
+    if mode == "train":
+        data_dir = sys.argv[2]
+        model_dir = sys.argv[3]
+        train(data_dir=data_dir, model_dir=model_dir)
+    elif mode == "test":
+        model_dir = sys.argv[2]
+        in_path = sys.argv[3]
+        out_path = sys.argv[4]
+        test(model_dir=model_dir, in_path=in_path, out_path=out_path)
+    else:
+        raise ValueError("Invalid mode (arg 1): '%s'. Expecting [train|test]" % mode)
+
