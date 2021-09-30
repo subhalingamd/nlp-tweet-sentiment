d1bf32b7a0aa79616975fcc1354ed3e98e8f043e
Subhalingam D <subhalingam.d@gmail.com>
2021-09-30 12:55:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
-from sklearn.model_selection import train_test_split
-from constants import RANDOM_SEED
+def read_test_file(path):
+    tweets = []
+    with open(path, 'r', encoding='utf-8', errors='ignore') as f:
+        for line in f:
+            line = line.strip()
+            tweets.append(line)
+        return tweets
 
 
-def read_file(path):
-    tweets, sentiments = [], []  # noqa: W191
+def read_train_file(path):
+    tweets, sentiments = [], []
     with open(path, 'r', encoding='utf-8', errors='ignore') as f:
         for line in f:
             line = line.strip()
@@ -16,7 +21,14 @@ def read_file(path):
         return tweets, sentiments
 
 
+def read_file(path):
+    return read_train_file(path)
+
+
 def data_split(X, y):
+    from sklearn.model_selection import train_test_split
+    from constants import RANDOM_SEED
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=RANDOM_SEED)
     X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, stratify=y_test, random_state=RANDOM_SEED)
     return (X_train, y_train), (X_val, y_val), (X_test, y_test)
@@ -25,3 +37,8 @@ def data_split(X, y):
 def read_file_and_split(path):
     X, y = read_file(path)
     return data_split(X, y)
+
+
+def write_predictions(path, arr):
+    with open(path, 'w', encoding='utf-8', errors='ignore') as f:
+        print(*arr, sep='\n', file=f)
