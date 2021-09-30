445242e69e94d1fe21755c707c953a4043d1939e
Subhalingam D <subhalingam.d@gmail.com>
2021-09-30 14:47:59 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -8,10 +8,10 @@ REGEX = {
     "unicode": re.compile(r'((?:\\u[0-9A-Fa-f]+)|(?:[^\x00-\x84]))'),
     "hashtag": re.compile(r"#(\w+)"),
     "mention": re.compile(r"@(\w+)"),
-    "url": re.compile(r"(?:http|https|ftp)://[a-zA-Z0-9\./]+"),
+    "url": re.compile(r"(?:https?|ftp)://[a-zA-Z0-9\./]+"),
     "repeat": re.compile(r"(.)\1{2,}", flags=re.IGNORECASE),
     "delimiter": re.compile(r'\W+'),
-    "number": re.compile(r"\s\d+\s"),
+    "number": re.compile(r"(?:^|\W)\d+(?:\W|$)"),
     "emotes": {},
     "punctuations": {}
 }
@@ -40,9 +40,50 @@ PUNCTUATIONS = [
                     ('__PUNC__EXCL',     ['!', '¡', ]),
                     ('__PUNC__QUES',     ['?', '¿', ]),
                     ('__PUNC__ELLP',     ['...', '…', '. . .', ]),
-                    # TODO : http://en.wikipedia.org/wiki/Punctuation
                 ]
 for name, symbols in PUNCTUATIONS:
     for symbol in symbols:
         REGEX['punctuations'].update({symbol: name})
 
+
+CONTRACTIONS = [
+                            (r'won\'t', 'will not'),
+                            (r'can\'t', 'can not'),
+                            (r'i\'m', 'i am'),
+                            (r'ain\'t', 'is not'),
+                            (r'(\w+)\'ll', '\g<1> will'),
+                            (r'(\w+)n\'t', '\g<1> not'),
+                            (r'(\w+)\'ve', '\g<1> have'),
+                            (r'(\w+)\'s', '\g<1> is'),
+                            (r'(\w+)\'re', '\g<1> are'),
+                            (r'(\w+)\'d', '\g<1> would'),
+                            (r'&', 'and'),
+                            (r'dont', 'do not'),
+                            (r'wont', 'will not'),
+                ]
+
+
+escape_for_regex = {
+                    '-': '\-',
+                    '/': '\/',
+                    ':': '\:',
+                    ')': '\)', 
+                    '@': '\@', 
+                    '<': '\<',
+                    ' ': '\s',
+                    '=': '\=',
+                    '_': '\_',
+                    ';': '\;',
+                    '[': '\[',
+                    '^': '\^',
+                    '*': '\*',
+                    ']': '\]',
+                    '(': '\(',
+                    '\\': '\\\\',
+                    '>': '\>',
+                    ',': '\,',
+                    '.': '\.',
+                    '!': '\!',
+                    '?': '\?',
+                    '+': '\+'
+                    }
